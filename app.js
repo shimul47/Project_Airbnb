00098b2e3606e3feda7d7fe1a9748efc5c7997a1
shimul47 <oshimul54@gmail.com>
2025-04-25 20:33:33 +0600

    April 25

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {istingSchema} = require("./schema.js");
 
 main().then(()=>{
     console.log("connceted")
@@ -40,6 +43,18 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+//here we are using Joi api as a function to identify error
+const validatelisting = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    // console.log(result);
+    if (error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
 //index route
 app.get("/listings",async(req,res)=> {
     const allListings = await Listing.find({});
@@ -61,30 +76,43 @@ app.get("/listings/:id", async(req,res)=>{
 });
 
 //create new route
-app.post("/listings",async(req,res)=>{
+app.post("/listings",validatelisting,
+    wrapAsync(async(req,res,next)=>{
+    
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+     
+    
+}));
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validatelisting,
+    wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`); 
-});
+}));
 //delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(`Given Data you have been deleted${deletedListing}`);
     res.redirect("/listings");
+}));
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found"));
+})
+app.use((err,req,res,next)=>{
+    let{statusCode,message} = err;
+    res.status(statusCode).render("Error.ejs",{err});
+    // res.status(statusCode=500).send(message="Page not found");
 })
 app.listen(8080,()=>{
     console.log(`App is listening to port 8080`);
