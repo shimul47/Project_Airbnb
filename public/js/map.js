071e196646b9d617c16e5959776ee2428f561921
shimul47 <oshimul54@gmail.com>
2025-05-29 17:31:41 +0600

    pushing again

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,12 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: "mapbox://styles/mapbox/streets-v12",
-    center: [77.209,28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 6
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: coordinates,
+  zoom: 9,
 });
-console.log(coordinates);
-const marker = new mapboxgl.Marker()
-    .setLngLat(coordinates)
-    .addTo(map)
+
+new mapboxgl.Marker()
+  .setLngLat(coordinates)
+  .addTo(map);
